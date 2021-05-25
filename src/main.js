09e8c929047d976176a19ecc40ec45174164cbfb
Yakoub Ouldbouchiba <yakoubOuldbouchiba>
2021-05-25 11:39:07 +0100

    add a vue files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
+
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart, faDollarSign);
+
+import Products from "./components/Products.vue";
+import Checkout from "./components/Checkout.vue";
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: "*",
+      component: Products
+    },
+    {
+      path: "/checkout",
+      component: Checkout
+    }
+  ]
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  router
+}).$mount("#app");
